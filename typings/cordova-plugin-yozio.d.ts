0a52cf7d034408bee4a099c40dd31c5e460356ce
Justin Unterreiner <justin.unterreiner@11main.com>
2016-03-09 08:54:32 -0800

    Version bump to 2.0.0

--- typings/cordova-plugin-yozio.d.ts
+++ typings/cordova-plugin-yozio.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for cordova-plugin-yozio 1.0.3
+// Type definitions for cordova-plugin-yozio 2.0.0
 // Project: https://github.com/Justin-Credible/cordova-plugin-yozio
 // Definitions by: Justin Unterreiner <https://github.com/Justin-Credible>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
